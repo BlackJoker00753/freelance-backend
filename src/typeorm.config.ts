934908e142da8eb3980633db56e5137c47f44b26
tan <tanat8511@gmail.com>
2024-06-10 11:10:10 +0500

    changed locally

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as process from "process";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: parseInt(process.env.POSTGRES_PORT, 10) || 5434,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || '123',
-  database: process.env.POSTGRES_DB || 'nest',
+  host: process.env.POSTGRES_HOST || 'balarama.db.elephantsql.com',
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+  username: process.env.POSTGRES_USER || 'ukgdgdrh',
+  password: process.env.POSTGRES_PASSWORD || '9wJRmhJgEZxHzYoPLH9HiuUJO-trWU0Q',
+  database: process.env.POSTGRES_DB || 'ukgdgdrh',
   autoLoadEntities: true,
   synchronize: true, // Note: Set this to false in production
 };
